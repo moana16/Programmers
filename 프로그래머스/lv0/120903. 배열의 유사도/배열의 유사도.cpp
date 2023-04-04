42547fc2e6770354e4cf1206dc9d0e4b961d1cac
moana16 <65649035+moana16@users.noreply.github.com>
2023-04-04 17:13:38 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+    for(auto str1 : s1) {
+        for(auto str2 : s2)  {
+            if(str1.length() != str2.length()) continue;
+            else {
+                if(str1.compare(str2)==0) answer++;
+            }
+        }
+    }
+    return answer;
+}
