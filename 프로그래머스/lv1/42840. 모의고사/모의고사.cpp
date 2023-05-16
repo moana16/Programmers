cd9bd7b891ca31c851fa6bd94f9231927de9210d
moana16 <65649035+moana16@users.noreply.github.com>
2023-05-16 20:49:28 +0900

    [level 1] Title: 모의고사, Time: 0.07 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
@@ -1,34 +1,26 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> pattern1 = {1, 2, 3, 4, 5};
-    vector<int> pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    vector<int> pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    vector<int> scores(3);
-
-    for (int i = 0; i < answers.size(); i++) {
-        if (answers[i] == pattern1[i % pattern1.size()]) {
-            scores[0]++;
-        }
-        if (answers[i] == pattern2[i % pattern2.size()]) {
-            scores[1]++;
-        }
-        if (answers[i] == pattern3[i % pattern3.size()]) {
-            scores[2]++;
-        }
+    vector<int> v1 = {1,2,3,4,5};
+    vector<int> v2 = {2,1,2,3,2,4,2,5};
+    vector<int> v3 = {3,3,1,1,2,2,4,4,5,5};
+    
+    int total_v1 = 0;
+    int total_v2 = 0;
+    int total_v3 = 0;
+    
+    for(int i=0; i<answers.size(); i++) {
+        if(v1[i%v1.size()] == answers[i]) total_v1++;
+        if(v2[i%v2.size()] == answers[i]) total_v2++;
+        if(v3[i%v3.size()] == answers[i]) total_v3++;
     }
-
-    int maxScore = *max_element(scores.begin(), scores.end());
-    for (int i = 0; i < scores.size(); i++) {
-        if (scores[i] == maxScore) {
-            answer.push_back(i + 1);
-        }
-    }
-
+    cout<<total_v2;
+    int maxNum = max({total_v1, total_v2, total_v3});
+    if(maxNum == total_v1) answer.push_back(1);
+    if(maxNum == total_v2) answer.push_back(2);
+    if(maxNum == total_v3) answer.push_back(3);
     return answer;
 }
