a7cb292c50158aa55ac5c9ef838874bd55de5563
moana16 <65649035+moana16@users.noreply.github.com>
2023-10-18 21:52:20 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,12 @@
+function solution(left, right) {
+    var answer = 0;
+    for(let i = left; i<= right; i++) {
+        let cnt = 0;
+        for(let j = 0; j <= i; j++) {
+            if(i % j === 0) cnt++;
+        }
+        cnt%2 === 0 ? answer += i : answer -= i;
+    }
+    
+    return answer;
+}
