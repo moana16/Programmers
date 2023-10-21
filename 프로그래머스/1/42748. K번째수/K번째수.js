cbc8f0a6cae82ab3666a7cbe2612ce027952b8bf
moana16 <65649035+moana16@users.noreply.github.com>
2023-10-21 17:16:40 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,6 @@
 function solution(array, commands) {
-    var answer = [];
-    for(c of commands) {
-        let arr = array.slice(c[0]-1, c[1]);
-        console.log(arr);
-        arr.sort((x,y)=>x-y);
-        answer.push(arr[c[2]-1]);
-    }
-    return answer;
+    return commands.map( c => {
+        return array.slice(c[0]-1, c[1]).sort((x,y)=>x-y)[c[2]-1];
+    })
+    
 }
