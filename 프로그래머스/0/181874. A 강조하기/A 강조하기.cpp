a31c0b304a5a18b73b01467dbde93be1bcd30a30
김지민 <65649035+moana16@users.noreply.github.com>
2024-03-18 15:57:53 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(auto c : myString) {
+        if(c == 'a' || c == 'A') answer += 'A';
+        else answer += tolower(c);
+    }
+    return answer;
+}
