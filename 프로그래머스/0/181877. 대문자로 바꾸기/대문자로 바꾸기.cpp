586a0b675cba261d3d1f9302d6e45e800cb028a0
김지민 <65649035+moana16@users.noreply.github.com>
2024-03-19 15:50:30 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(auto c : myString) {
+        answer += toupper(c);
+    }
+    return answer;
+}
