618e76f7fde1c85d08fa51ccee439d3158373718
김지민 <65649035+moana16@users.noreply.github.com>
2024-03-30 18:09:52 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.39 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -1,16 +1,13 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(string myString, string pat) {
-    int answer = 0;
-    if(myString.size() < pat.size()) return 0;
-    string a,b;
-    for(auto c : myString) a += tolower(c); 
-    for(auto c : pat) b += tolower(c); 
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
     
-    if(a.find(b) == string::npos) return 0;
+    if(myString.find(pat) == string::npos) return 0;
     else return 1;
-    return answer;
 }
