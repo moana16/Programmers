064242962810c254e6f1f5cd0e8910b8e50d5d5e
김지민 <65649035+moana16@users.noreply.github.com>
2024-08-12 13:45:44 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    for(auto s : my_string) {
+        for(int i=0; i<n; i++) answer += s;
+    }
+    return answer;
+}
