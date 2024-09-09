3fe326bd8ea901ec952b6b25a58141f4372d6c78
김지민 <65649035+moana16@users.noreply.github.com>
2024-09-09 17:23:22 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    for(auto num : num_list ) {
+        if(num == n) return 1;
+    }
+    return 0;
+}
