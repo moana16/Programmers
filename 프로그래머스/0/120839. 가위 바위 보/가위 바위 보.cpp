6bf9c4ce79cc3ded612a5b4623de0f82c6dd6859
김지민 <65649035+moana16@users.noreply.github.com>
2025-02-16 11:02:34 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for(auto c : rsp) {
+        if(c == '0') answer += '5';
+        else if(c == '2') answer += '0';
+        else answer += '2';
+    }
+    return answer;
+}
