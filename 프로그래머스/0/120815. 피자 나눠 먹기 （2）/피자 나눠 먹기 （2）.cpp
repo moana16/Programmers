d5290a6e7f672b3aeb6298d16a6f716828f8ff53
김지민 <65649035+moana16@users.noreply.github.com>
2025-03-21 21:18:59 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1; i<=100; i++) {
+        if(6 * i % n == 0) return i;
+    }
+    return answer;
+}
