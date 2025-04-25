3aa5088ff36eea1ee27f55ed1e92cc5e1872cf21
김지민 <65649035+moana16@users.noreply.github.com>
2025-04-25 16:17:36 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 4.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    vector<int> answer;
+    if(n == 1) {
+        answer.assign(num_list.begin(), num_list.begin() + slicer[1]+1); 
+    }else if(n == 2) {
+        answer.assign(num_list.begin()+slicer[0], num_list.end()); 
+        
+    }else if(n == 3) {
+        answer.assign(num_list.begin()+slicer[0], num_list.begin() + slicer[1]+1); 
+    }else {
+        for(int i=slicer[0]; i<=slicer[1]; i+= slicer[2]) answer.push_back(num_list[i]);
+    }
+    return answer;
+}
