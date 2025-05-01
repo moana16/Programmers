ebcd8bdd47004a0c03aa66a869a1b28a8be612d7
김지민 <65649035+moana16@users.noreply.github.com>
2025-05-01 19:32:24 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer(52);
+    for(auto c : my_string) {
+        if(c >= 'A' && c <= 'Z') answer[c-'A']++;
+        else if(c >= 'a' && c <= 'z') answer[c-'a'+26]++;
+    }
+    return answer;
+}
